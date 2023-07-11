0421d97d905849782ac14fc854ab1a5a2a9585c0
Muhammed Uludağ <103314663+Muhammeduludag-web@users.noreply.github.com>
2023-07-11 20:55:49 +0300

    Project-Layout

---
+++ 4- ProjectLayout/ProjectLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by uludağ on 12.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
